0de6d4adab9c3a6e5e9baa1462328006f6cde504
Tomson Kan <tomson.kan@gmail.com>
2019-07-29 20:56:49 -0500

    upload

---
+++ code-along/javascript/app.js
@@ -0,0 +1,7 @@
+var  myName = 'Tomson Kan'
+
+console.log(myName)
+console.log('myName:', myName)
+var nameElement = document.getElementsByClassName('name') [0]
+// console.log('nameElement:',nameElement)
+nameElement.innerHTML = myName
